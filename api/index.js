81521cb941fe77135b0e576c32ceac3b9a9ec219
Maseyfacey <153440666+Maseyfacey@users.noreply.github.com>
2026-03-25 11:22:18 -0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,11 +2,17 @@ import { createBareServer } from "@tomphttp/bare-server-node";
 
 const bare = createBareServer("/bare/");
 
-export default function handler(req, res) {
-  if (bare.shouldRoute(req)) {
-    bare.routeRequest(req, res);
-  } else {
-    res.statusCode = 404;
-    res.end("Not found");
+export default async function handler(req, res) {
+  try {
+    if (bare.shouldRoute(req)) {
+      await bare.routeRequest(req, res);
+    } else {
+      res.statusCode = 404;
+      res.end("Not found");
+    }
+  } catch (err) {
+    console.error("Bare server error:", err);
+    res.statusCode = 500;
+    res.end("Server error");
   }
 }
